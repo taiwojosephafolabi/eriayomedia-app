63c623deb1c8b46d605c5b5021dde47ef703b86c
JTAfolabi <taiwojosephafolabi@gmail.com>
2023-10-17 02:32:25 +0100

    landing page updates with logo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
+import logo from './images/Ẹríayọ̀-Media-Logo.png';
 
 function App() {
   return (
     <div className='App'>
+      <header>
+        <img src={logo} alt='Ẹríayọ̀ Media - Logo' />
+      </header>
       <p>SITE COMING SOON</p>
+      
       <a
         className='App-link'
         href='https://www.instagram.com/eriayomedia'
         target='_blank'
         rel='noopener noreferrer'
       >
-        Instagram
+       Instagram
       </a>
     </div>
   );
